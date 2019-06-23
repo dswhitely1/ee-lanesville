40bf5d42333a927c6bb22c98ba44d7e6897f9884
Donald Whitely <dswhitely1@gmail.com>
2019-06-22 22:32:00 -0400

    Cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import Navigation from './sharedComponents/Navigation';
 import Footer from "./sharedComponents/Footer";
-
+import LandingPage from './landingPage/LandingPage';
 const App = () => {
     return (
         <>
             <Navigation/>
+            <Route exact path='/' component={LandingPage} />
             <Footer/>
         </>
     )
